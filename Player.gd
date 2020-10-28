27c0f514120ee1819c43fe05b4b398ca3c2b983e
Pikmin4please <73556644+Pikmin4please@users.noreply.github.com>
2020-10-29 00:11:53 +0200

    Continued player gameplay

--- Player.gd
+++ Player.gd
@@ -1,12 +1,14 @@
 extends KinematicBody2D
 
 export (int) var speed = 100
-export (int) var jump_speed = 50 
-export (int) var gravity = 25
+export (int) var jump_speed = -400
+export (int) var gravity = 1200
+
 
 var velocity = Vector2.ZERO
 
 func get_input():
+	velocity = Vector2.ZERO
 	if Input.is_action_pressed("ui_right"):
 		velocity.x = speed
 	if Input.is_action_pressed("ui_left"):
@@ -14,7 +16,8 @@ func get_input():
 	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = jump_speed
 
-func _process(delta):
+func _physics_process(delta):
 	get_input()
+	print(is_on_floor())
 	velocity.y += gravity * delta
 	velocity = move_and_slide(velocity, Vector2(0,-1))
