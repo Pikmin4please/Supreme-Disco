bf2585e8147c6d04460fe5dc11a66512ee17a279
Pikmin4please <73556644+Pikmin4please@users.noreply.github.com>
2020-10-28 23:12:34 +0200

    Updated Player controls

--- Player.gd
+++ Player.gd
@@ -1,11 +1,20 @@
-extends Area2D
+extends KinematicBody2D
 
 export (int) var speed = 100
 export (int) var jump_speed = 50 
+export (int) var gravity = 25
 
 var velocity = Vector2.ZERO
 
 func get_input():
-	var input = velocity
-	if Input.is_action_just_pressed("ui_right"):
-		input.x = 
+	if Input.is_action_pressed("ui_right"):
+		velocity.x = speed
+	if Input.is_action_pressed("ui_left"):
+		velocity.x = -speed
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+		velocity.y = jump_speed
+
+func _process(delta):
+	get_input()
+	velocity.y += gravity * delta
+	velocity = move_and_slide(velocity, Vector2(0,-1))
