448f49b41d344080303a622ed734e6b4e96296d5
Pikmin4please <73556644+Pikmin4please@users.noreply.github.com>
2020-10-28 19:45:58 +0200

    Added basic movements to player

--- Player.gd
+++ Player.gd
@@ -1,2 +1,14 @@
 extends Area2D
 
+var speed = 100
+
+
+func _physics_process(delta):
+	if Input.is_action_pressed("ui_right"):
+		move(speed,delta)
+	if Input.is_action_pressed("ui_left"):
+		move(-speed, delta)
+
+
+func move(xspeed,delta):
+	position.x += xspeed * delta
