a552713f48fa3e97b97d46a71d874cfed36d8121
Pikmin4please <73556644+Pikmin4please@users.noreply.github.com>
2020-10-31 00:31:20 +0200

    Finished Basic player movements

--- Player.gd
+++ Player.gd
@@ -1,23 +1,30 @@
 extends KinematicBody2D
 
-export (int) var speed = 100
 export (int) var jump_speed = -400
-export (int) var gravity = 1200
+export (int) var gravity = 1100
+export (int) var MAX_SPEED = 150
+export (float) var FRICTION = 0.2
+export (float) var acceleration = 0.8
+
 
 
 var velocity = Vector2.ZERO
 
 func get_input():
-	velocity = Vector2.ZERO
+	velocity.x = 0
+	var moving = false
 	if Input.is_action_pressed("ui_right"):
-		velocity.x = speed
+		velocity.x = lerp(velocity.x, MAX_SPEED,acceleration)
+		moving = true
 	if Input.is_action_pressed("ui_left"):
-		velocity.x = -speed
-	if Input.is_action_just_pressed("ui_up") and is_on_floor():
-		velocity.y = jump_speed
+		velocity.x = lerp(velocity.x,-MAX_SPEED, acceleration)
+		moving = true
+	if moving == false:
+		velocity.x = lerp(velocity.x, 0, FRICTION)
 
 func _physics_process(delta):
 	get_input()
-	print(is_on_floor())
 	velocity.y += gravity * delta
 	velocity = move_and_slide(velocity, Vector2(0,-1))
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+		velocity.y = jump_speed 
