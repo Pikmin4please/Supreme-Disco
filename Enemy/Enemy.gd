bc1ced4b7ca4024620d28a8f05e150c0834c81be
kostor <yardensnw@gmail.com>
2020-11-02 22:49:27 +0200

    Enemy behavior test 1

--- Enemy/Enemy.gd
+++ Enemy/Enemy.gd
@@ -4,9 +4,17 @@ export (int) var MAX_SPEED = 150
 export (float) var FRICTION = 0.2
 export (float) var acceleration = 0.8
 
-var velocity = Vector2.ZERO
 
+#onready var WallLeft = $WallLeft;
+
+var velocity = Vector2.ZERO;
 var dir = true;
+
+
+func _ready():
+	velocity = Vector2(1,0);
+
+
 
 
 # This function is to be called when the enemy reaches one of its boundaries and is required to change direction of moving.
@@ -14,6 +22,17 @@ func ChangeDir():
 	dir = !dir;
 
 func _physics_process(delta):
-	velocity.x = lerp(velocity.x, MAX_SPEED,acceleration);
+	print(-1)
+	var collision = move_and_collide(velocity,true,false);
+	print(0)
+	if (collision):
+		print(1)
+		var collider = collision.get_collider();
+		var collider_type = collider.collision_mask;
+		print(collider_type)
+		if(collider_type==28):
+			ChangeDir();
 
-CollisionObject2D.signal
+	#velocity.x = lerp(velocity.x, MAX_SPEED,acceleration);
+
+
