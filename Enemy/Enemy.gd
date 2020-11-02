d26ed43d54d558c8caf85bf37ed8f52bb11211ca
kostor <yardensnw@gmail.com>
2020-11-02 23:21:28 +0200

    Enemy movement behavior finished

--- Enemy/Enemy.gd
+++ Enemy/Enemy.gd
@@ -1,41 +1,29 @@
 extends KinematicBody2D
 
-export (int) var MAX_SPEED = 150
-export (float) var FRICTION = 0.2
-export (float) var acceleration = 0.8
-
-
-#onready var WallLeft = $WallLeft;
+export (int) var EnemySpeed = 3;
 
 var velocity = Vector2.ZERO;
 var dir = true;
 
-
 func _ready():
-	velocity = Vector2(10,0);
-
-
+	velocity = Vector2(EnemySpeed,0);
 
 
 # This function is to be called when the enemy reaches one of its boundaries and is required to change direction of moving.
 func ChangeDir():
 	dir = !dir;
 	if(dir):
-		velocity = Vector2(1,0);
+		velocity = Vector2(EnemySpeed,0);
 	else:
-		velocity = Vector2(-1,0);
+		velocity = Vector2(-EnemySpeed,0);
 
 func _physics_process(delta):
 	
-	var collision = move_and_collide(velocity,true,false); # Moves the enemy body to the right (by defualt) untill a colission is detected
+	# Moves the enemy body to the right (by defualt) untill a colission is detected (returns null if none is detected)
+	var collision = move_and_collide(velocity,true,false);
 	
 	if (collision):
 		var collider = collision.get_collider();
-		var collider_type = collider.collision_mask;
-		print(collider_type)
-		if(collider_type == 28):
+		var collider_type = collider.collision_layer;
+		if(collider_type == 1):
 			ChangeDir();
-
-	#velocity.x = lerp(velocity.x, MAX_SPEED,acceleration);
-
-
