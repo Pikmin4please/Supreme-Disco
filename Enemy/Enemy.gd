00fde8e5ed1164c30801fca48e830fe869a9bffc
kostor <yardensnw@gmail.com>
2020-11-02 23:04:47 +0200

    Enemy Behavior 2nd Test

--- Enemy/Enemy.gd
+++ Enemy/Enemy.gd
@@ -12,7 +12,7 @@ var dir = true;
 
 
 func _ready():
-	velocity = Vector2(1,0);
+	velocity = Vector2(10,0);
 
 
 
@@ -20,17 +20,20 @@ func _ready():
 # This function is to be called when the enemy reaches one of its boundaries and is required to change direction of moving.
 func ChangeDir():
 	dir = !dir;
+	if(dir):
+		velocity = Vector2(1,0);
+	else:
+		velocity = Vector2(-1,0);
 
 func _physics_process(delta):
-	print(-1)
-	var collision = move_and_collide(velocity,true,false);
-	print(0)
+	
+	var collision = move_and_collide(velocity,true,false); # Moves the enemy body to the right (by defualt) untill a colission is detected
+	
 	if (collision):
-		print(1)
 		var collider = collision.get_collider();
 		var collider_type = collider.collision_mask;
 		print(collider_type)
-		if(collider_type==28):
+		if(collider_type == 28):
 			ChangeDir();
 
 	#velocity.x = lerp(velocity.x, MAX_SPEED,acceleration);
