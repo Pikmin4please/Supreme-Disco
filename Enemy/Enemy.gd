932a6c9da6899885992cad1fb9cf4c8c03ad3915
kostor <yardensnw@gmail.com>
2020-11-03 10:37:00 +0200

    Added some comments on Enemy.gd

--- Enemy/Enemy.gd
+++ Enemy/Enemy.gd
@@ -1,15 +1,19 @@
 extends KinematicBody2D
 
+# Enemy horizontal speed
 export (int) var EnemySpeed = 3;
 
+#Enemy 
 var velocity = Vector2.ZERO;
 var dir = true;
 
+# This function sets the defualt velocity of the enemy.
 func _ready():
 	velocity = Vector2(EnemySpeed,0);
 
 
-# This function is to be called when the enemy reaches one of its boundaries and is required to change direction of moving.
+# This function is to be called when the enemy reaches one of its boundaries 
+# and is required to change direction of its movement.
 func ChangeDir():
 	dir = !dir;
 	if(dir):
@@ -17,13 +21,19 @@ func ChangeDir():
 	else:
 		velocity = Vector2(-EnemySpeed,0);
 
-func _physics_process(delta):
-	
-	# Moves the enemy body to the right (by defualt) untill a colission is detected (returns null if none is detected)
+# This is the physics tick function
+func _physics_process(delta):	
+	# Moves the enemy body to the default direction untill a colission is 
+	# detected. It'll return null if no collision is detected
 	var collision = move_and_collide(velocity,true,false);
-	
+	# Check if collision isn't null. If it isn't null, start processing the 
+	# collision data
 	if (collision):
+		# Get the coliider - The object the enemy is colliding with
 		var collider = collision.get_collider();
+		# Understand what is the layer of the collider. It is set as such that
+		# all the walls have their own seperate layer
 		var collider_type = collider.collision_layer;
+		# Thus if it the wall layer, it is surely a wall.
 		if(collider_type == 1):
-			ChangeDir();
+			ChangeDir(); # Change direction due to contact with a wall.
